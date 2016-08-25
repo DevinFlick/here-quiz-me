c5870886b712613a0bc29e727ff04c15f3b629d9
DevinFlick <DevinJFlick@gmail.com>
2016-08-25 18:55:36 -0400

    added auth functionality to navbar

--- public/js/controllers/navbarController.js
+++ public/js/controllers/navbarController.js
@@ -1,11 +1,18 @@
 (function() {
   angular.module('ngQuiz')
           .controller('navbarController', navbarController);
-navbarController.$inject = ['$scope', '$location'];
+navbarController.$inject = ['$scope', '$location', 'AuthService'];
 
-function navbarController($scope, $location){
+function navbarController($scope, $location, AuthService){
   $scope.isActive = function (viewLocation){
     return viewLocation === $location.path();
   };
+  $scope.isLoggedIn = AuthService.isLoggedIn;
+  $scope.logout = logout;
+  $scope.user = AuthService.currentUser();
+
+  function logout(){
+    AuthService.logout();
+  }; //done for future functionality
 };
 }());
