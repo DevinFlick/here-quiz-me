5c30fa57e1901bc19e7df5215942623232d711e1
DevinFlick <DevinJFlick@gmail.com>
2016-08-26 21:00:45 -0400

    things kinda work!

--- public/js/services/UserService.js
+++ public/js/services/UserService.js
@@ -7,7 +7,7 @@
   function UserService($http){
     var baseUrl = 'https://quiz-base.herokuapp.com/users/';
     var signUpUrl = 'https://quiz-base.herokuapp.com/signup/';
-    
+
     var service = {
       getAllUsers: getAllUsers,
       signUp: signUp,
@@ -19,18 +19,18 @@
 
     function getAllUsers(){
       return $http.get(baseUrl);
-    };
+    }
     function signUp(userObj){
       return $http.post(signUpUrl, userObj);
-    };
+    }
     function getOneUser(userId){
       return $http.get(baseUrl + userId);
-    };
+    }
     function updateUser(userId, updateInfo){
       return $http.update(baseUrl, updateInfo);
-    };
+    }
     function deleteUser(userId){
-      return $http.delete(baseUrl + userId)
-    };
+      return $http.delete(baseUrl + userId);
+    }
   }
 }());
